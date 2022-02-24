32c2a7565d659eef0667a49f15ecf1eaec7913ea
mikeyolang <brunomike965@gmail.com>
2022-02-24 17:29:07 +0300

    Initial commit

---
+++ Music player/data.js
@@ -0,0 +1,34 @@
+//  Contains music related data
+
+let songs = [
+    {
+        name: "A little bit yours",
+        path: "./Assets/Music/A Little Bit Yours - JP Saxe.m4a",
+        artist: "Jape Saxe",
+        cover: "./Assets/photos/alexander-shatov-qt7S2QuAhJI-unsplash.jpg"
+    },
+    {
+        name: "A little bit yours",
+        path: "./Assets/Music/A Little Bit Yours - JP Saxe.m4a",
+        artist: "Jape Saxe",
+        cover: "./Assets/photos/alexander-shatov-qt7S2QuAhJI-unsplash.jpg"
+    },
+    {
+        name: "A little bit yours",
+        path: "./Assets/Music/A Little Bit Yours - JP Saxe.m4a",
+        artist: "Jape Saxe",
+        cover: "./Assets/photos/alexander-shatov-qt7S2QuAhJI-unsplash.jpg"
+    },
+    {
+        name: "A little bit yours",
+        path: "./Assets/Music/A Little Bit Yours - JP Saxe.m4a",
+        artist: "Jape Saxe",
+        cover: "./Assets/photos/alexander-shatov-qt7S2QuAhJI-unsplash.jpg"
+    },
+    {
+        name: "A little bit yours",
+        path: "./Assets/Music/A Little Bit Yours - JP Saxe.m4a",
+        artist: "Jape Saxe",
+        cover: "./Assets/photos/alexander-shatov-qt7S2QuAhJI-unsplash.jpg"
+    },
+]
