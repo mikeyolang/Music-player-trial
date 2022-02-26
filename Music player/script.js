198388140eefd7df3b39c9e15e2d29f4c50fc934
mikeyolang <brunomike965@gmail.com>
2022-02-26 18:04:21 +0300

    Correctiion of the code

---
+++ Music player/script.js
@@ -0,0 +1,104 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+const songs = [
+    'A little Bit Yours- JP Saxe.m4a',
+    'All of Me- John Legend.m4a',
+    'One Last Time- Ariana Grande.m4a'
+]
+
+let songIndex = 2;
+// The Load song Function
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `./Assets/Music/${song}.m4a`;
+    cover.src = `./Assets/photos/${song}.jpg`;
+}
+// Making the play song function
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+// The playsong function
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+// The pause song function
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+// Implememnting the nexy and previous button
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Implement the progress bar
+audio.addEventListener('timeupdate', updateProgress);
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+progressContainer.addEventListener('click', setProgress);
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Playing th enext song automaticall
+audio.addEventListener('ended', nextSong);
+
+
